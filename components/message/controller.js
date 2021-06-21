a3a9b722f44cd9f73cb240024af0db73d3caaded
alexsc90 <a.s.cebreros@gmail.com>
2021-06-21 15:50:04 -0500

    codigo separado para rutas, controladores y base de datos

---
+++ components/message/controller.js
@@ -0,0 +1,31 @@
+const store = require('./store');
+
+function addMessage(user, message) {
+    return new Promise((resolve, reject) => {
+        if(!user || !message) {
+            console.log('[messageController] No hay usuario o mensaje')
+            reject('Datos incorrectos');
+            return false;
+        }
+        const fullMessage = {
+            user: user,
+            message: message,
+            date: new Date(),
+        };
+    
+        store.add(fullMessage);
+
+        resolve(fullMessage);
+    });
+}
+
+function getMessages() {
+    return new Promise((resolve, reject) => {
+        resolve(store.list());
+    })
+}
+
+module.exports = {
+    addMessage,
+    getMessages,
+}
--- server.js
+++ server.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const response = require ('./network/response');
+// const router = require('./components/message/network'); 
 
-const router = express.Router();
+const router = require('./network/routes');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(router);
+//app.use(router);
 
-/* app.use('/', (req, res) => {
-    res.send('Hola');
-}) */
+router(app);
 
-router.get('/message', (req, res) => {
-    console.log(req.headers);
-    res.header({
-        "custom-header": "Nuestro valor personalizado",
-    })
-    // res.send('Msg List');
-    response.success(req, res, 'Message List');
-});
-
-router.post('/message', (req, res) => {
-    // res.status(201).send([{error: '', body: 'Creado correctamente'}]);
-    if(req.query.error == 'ok') {
-        response.error(req, res, 'Error inesperado', 500, 'Es solo una simulacion de los errores');
-    } else {
-        response.success(req, res, 'Creado correctamente', 201)
-    }
-});
-
-router.delete('/message', (req, res) => {
-    console.log(req.query);
-    console.log(req.body);
-    res.send('Msg ' + req.body.text + ' deleted correctly');
-});
 
 app.use('/app', express.static('public'));
 
